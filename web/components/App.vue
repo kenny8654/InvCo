146712f0a57c3dd38965e90d657aa4ea63155d13
YiChen <kenny8654@gmail.com>
2019-11-22 17:06:26 +0800

    (Fix): Fix cannot upload image after clicking remove button

--- web/components/App.vue
+++ web/components/App.vue
@@ -59,9 +59,13 @@ export default {
   },
 
     removeUpload : function () {
-      $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+      // $('.file-upload-input').replaceWith($('.file-upload-input').clone());
       $('.file-upload-content').hide();
       $('.image-upload-wrap').show();
+      $('.file-upload-input').show()
+      $('.file-upload-input').click(function() {
+        alert( "Handler for .click() called." );
+      })
       console.log('remove btn');
   }}}
 
