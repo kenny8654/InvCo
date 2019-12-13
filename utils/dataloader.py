e34538c31498912e51df7518fcf582385b138fd2
Timmy <apsttimmy@gmail.com>
2019-12-13 14:12:29 +0800

    fix: import bug

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -4,7 +4,7 @@ import torch.utils.data as data
 import os
 import pickle as pk
 import numpy as np
-from build_vocab import Vocabulary
+from utils.ingrs_vocab import Vocabulary
 
 class RecipeDataset(data.Dataset):
     def __init__(self,dir_file,max_num_samples=-1,maxnumims=5,max_num_labels=20,split='train'):
