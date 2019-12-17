fd2367070904601558814ce13a9058db6a28f9b1
SWIvan <asd95175379@gmail.com>
2019-12-17 21:14:26 +0800

    (feat): get ingredient and unit

--- utils/ingrs_vocab.py
+++ utils/ingrs_vocab.py
@@ -2,37 +2,62 @@ import nltk
 import pickle
 import argparse
 from collections import Counter
+from itertools import zip_longest
 import json
 import os
+from tqdm import *
 import numpy as np
 import re
-from tqdm import tqdm
 nltk.download('punkt')
 # Local import
 from invco import DATASET_DIR
-
 
 def get_ingredient(det_ingr, replace_dict):
     det_ingr_lower = det_ingr['text'].lower()
     
     det_ingr_lower = ''.join(i for i in det_ingr_lower if not i.isdigit())
-    
-    #########################Layer1 ingredients##########################
-    #remove info in brackets
-    # regex = '\(.*?\)'
-    # det_ingr_lower = re.sub(regex,'',det_ingr_lower)
-    #####################################################################
 
     #replace word
     for rep_word, char_list in replace_dict.items():
         for char in char_list:
             if char in det_ingr_lower:
                 det_ingr_lower = det_ingr_lower.replace(char, rep_word)
+    
     #remove trailing newline
     det_ingr_lower = det_ingr_lower.strip()
     det_ingr_lower = det_ingr_lower.replace(' ', '_')
 
     return det_ingr_lower
+
+def get_units(det_unit, replace_word):
+    det_unit_lower = det_unit['text'].lower()
+    det_unit_lower = ''.join(i for i in det_unit_lower)
+
+    #remove info in brackets
+    regex = '\(.*?\)'
+    det_unit_lower = re.sub(regex,'',det_unit_lower)
+
+    # remove the redundant quantity
+    found_digit = False
+    ingrs = ''
+    for item in det_unit_lower.split(' '):
+        if not len(item) > 0:
+            continue
+        if not item[0].isdigit() or not found_digit:
+            found_digit = True
+            ingrs += item+' '
+    
+    #replace word
+    for rep_word, char_list in replace_word.items():
+        for char in char_list:
+            if char in ingrs:
+                ingrs = ingrs.replace(char, rep_word)
+    
+    #remove trailing newline
+    ingrs = ingrs.strip()
+    ingrs = ingrs.replace(' ', '_')
+
+    return ingrs
 
 def get_instruction(instr, replace_dict_instrs):
     instr_lower = instr['text'].lower()
@@ -48,11 +73,13 @@ def get_instruction(instr, replace_dict_instrs):
         instr_lower = ''
     return instr_lower
 
-def update_counter(list_, counter_toks, istrain=False):
+def update_counter(list_, counter_units, istrain=False):
+
     for sentence in list_:
+        sentence = sentence.replace('_', ' ')
         tokens = nltk.tokenize.word_tokenize(sentence)
         if istrain:
-            counter_toks.update(tokens)
+            counter_units.update(tokens)
 
 def cluster_vocab(counter_ingrs):
     ingr_mydict = dict()
@@ -94,9 +121,6 @@ def cluster_vocab(counter_ingrs):
         if not found:
             ingr_mydict[key] = value
             ingr_category[key] = [key]
-        
-    print('Size of pepper ingredient:', len(ingr_category['pepper']))
-    print('Size of cheese ingredient:', len(ingr_category['cheese']))
 
     return ingr_mydict,ingr_category
 
@@ -128,13 +152,22 @@ def remove_plurals(counter_ingrs, category_ingrs):
         del category_ingrs[item]
     return counter_ingrs, category_ingrs
 
+def plurals(ingr_candidate):
+    if ingr_candidate[-2:] == 'es':
+        ingr_candidate = ingr_candidate[:-2]
+
+    elif ingr_candidate[-1:] == 's':
+        ingr_candidate = ingr_candidate[:-1]
+    
+    return ingr_candidate
+
 def build_vocab(dir_file):
     
     #open file
     print('Loading data')
     dets = json.load(open(os.path.join(dir_file, 'det_ingrs.json'), 'r'))
     layer1 = json.load(open(os.path.join(dir_file, 'layer1.json'), 'r'))
-    layer2 = json.load(open(os.path.join(dir_file, 'layer2.json'), 'r'))
+    layer2 = json.load(open(os.path.join(dir_file, 'layer2+.json'), 'r'))
 
     #save rec_id via line
     id2im_pos = {}
@@ -142,71 +175,113 @@ def build_vocab(dir_file):
         id2im_pos[line['id']] = i
         
     id2ingr_pos = {}
-    for i, line in enumerate(dets):
+    for i, line in enumerate(layer1):
         id2ingr_pos[line['id']] = i
     
 
     print('Data loaded!')
 
     #replace instruction and ingredients words 
-    replace_Ingrs = {'and': ['&',"'n"], '':  ['%', ',', '.', '#', '[', ']', '!', '?']}
+    replace_Ingrs = {'and': ['&',"'n"], 'cup': ['c.', 'cups'], 'tablespoon': ['tbsp', 'tablespoons'], 'teaspoon': ['teaspoons', 'ts'], '':  ['%', ',', '.', '#', '[', ']', '!', '?']}
     replace_dict_instrs = {'and': ['&', "'n"], '': ['#', '[', ']']}
-    
+    # replace_units = {'and': ['&',"'n"], 'cup': ['c.', 'cups'], 'tablespoon': ['tbsp', 'tablespoons'], 'teaspoon': ['teaspoons', 'ts'], '':  ['%', ',', '.', '#', '[', ']', '!', '?', '-', 'to']}
+
     #Count words
     ingrs_file = os.path.join(dir_file, 'allingrs_count.pkl')
-    instrs_file = os.path.join(dir_file, 'allinstrs_count.pkl')
-
-    if os.path.exists(ingrs_file) and os.path.exists(instrs_file):
+    unit_file = os.path.join(dir_file, 'allingrs_unit.pkl')
+    
+    if os.path.exists(ingrs_file) and os.path.exists(unit_file):    
         print('File existed!')
         with open('allingrs_count.pkl', 'rb') as file:
             counter_ingrs = pickle.load(file)
-        with open('allinstrs_count.pkl', 'rb') as file:
-            counter_toks = pickle.load(file)
+        with open('allingrs_unit.pkl', 'rb') as file:
+            counter_units = pickle.load(file)
     else:
-        counter_toks = Counter()
+        dataset = {'train': [], 'val': [], 'test': []}
         counter_ingrs = Counter()
+        counter_units = Counter()
 
         for i, entry in tqdm(enumerate(layer1)):
             #get all instructions from the recipe
             instrs = entry['instructions']
 
             instrs_list = []
-            ingrs_lsit = []
+            ingrs_list = []
+            unit_list = []
+            images_list = []
 
             #get ingredients
             det_ingrs = dets[id2ingr_pos[entry['id']]]['ingredients']
             valid = dets[id2ingr_pos[entry['id']]]['valid']
+            det_units = layer1[id2ingr_pos[entry['id']]]['ingredients']
+            ingrs_data = zip_longest(det_ingrs,det_units)
 
-            for j,det_ingr in enumerate(det_ingrs):
+            for j, (det_ingr,det_unit) in enumerate(ingrs_data):
                 if len(det_ingr) > 0 and valid[j]:
-                    ingrs_lsit.append(get_ingredient(det_ingr, replace_Ingrs))
-            
-            #get instructions
+                    ingr_candidate = get_ingredient(det_ingr, replace_Ingrs)
+                    ingr_candidate = plurals(ingr_candidate)
+                    ingrs_list.append(ingr_candidate)
+                    unit_lower = get_units(det_unit, replace_Ingrs)
+                    if ingr_candidate in unit_lower:
+                        unit_candidate = simplify_ingrs(unit_lower, ingr_candidate)
+                        unit_list.append(unit_candidate)
+                    else:
+                        unit_candidate = '1_' + ingr_candidate
+                        unit_list.append(unit_candidate)
+
+            #get instruction
             acc_len = 0 #Calculate length of instruction
             for j,instr in enumerate(instrs):
                 tmp = get_instruction(instr, replace_dict_instrs)
                 if len(tmp) > 0:
                     instrs_list.append(tmp)
-                    # acc_len += len(tmp)
+                    acc_len += len(tmp)
+
             
-            #remove recipes with too few or too many ingredients or instruction
-            if len(ingrs_lsit) < 2 or len(instrs_list) < 2 \
-                    or len(ingrs_lsit) > 20 or len(instrs_list) >20:
+            # we discard recipes with too many or too few ingredients or instruction words
+            if len(ingrs_list) < 2 or len(instrs_list) < 2 \
+                    or len(instrs_list) >= 20 or len(ingrs_list) >= 20 \
+                    or acc_len < 20:
                 continue
 
+            if entry['id'] in id2im_pos.keys():
+                ims = layer2[id2im_pos[entry['id']]]
+
+                # copy image paths for this recipe
+                for im in ims['images']:
+                    images_list.append(im['id'])
+
             #tokenize sentences and update counter
-            update_counter(instrs_list, counter_toks, istrain=entry['partition'] == 'train')
+            # update_counter(instrs_list, counter_toks, istrain=entry['partition'] == 'train')
+            update_counter(unit_list, counter_units, istrain=entry['partition'] == 'train')
+            title = nltk.tokenize.word_tokenize(entry['title'].lower())
+
             if entry['partition'] == 'train':
-                counter_ingrs.update(ingrs_lsit)
+                counter_ingrs.update(ingrs_list)
+            if entry['partition'] == 'train':
+                counter_units.update(title)
+            
+
+            newentry = {'id': entry['id'], 'ingredients': ingrs_list,
+                        'ingrs_unit': unit_list,
+                        'images': images_list, 'title': title}
+            dataset[entry['partition']].append(newentry)
         
         pickle.dump(counter_ingrs, open(ingrs_file, 'wb'))
-        pickle.dump(counter_toks, open(instrs_file, 'wb'))
+        pickle.dump(counter_units, open(unit_file, 'wb'))
     
-    #Pre-custom thesaurus
+    print('Dataset size:')
+    for split in dataset.keys():
+        print(split, ':', len(dataset[split]))    
+
+    
+    
+    # Pre-custom thesaurus
     with open ('ingr_vocab.pkl', 'rb') as file:
         base_words = pickle.load(file)
 
     counter_ingrs.update(base_words)
+    counter_units.update(base_words)
 
     #simplified ingredient
     print('original data %d' %(len(counter_ingrs)))
@@ -216,28 +291,29 @@ def build_vocab(dir_file):
     print('simplified ingredient %d' %(len(counter_ingrs)))
 
     # If the word frequency is less than '10', then the word is discarded.
-    words = [word for word, count in counter_toks.items() if count >= 10]
     ingrs = {word: count for word, count in counter_ingrs.items() if count >= 10}
+    unit = [word for word, cnt in counter_units.items() if cnt >= 10]
 
     print('Length of ingredients %d' % (len(ingrs)))
+    print('Length of unit ingredients %d' % (len(unit)))
 
-
-    # Recipe vocab
+    # Recipe unit vocab
     # Create a vocab wrapper and add some special tokens.
-    vocab_toks = Vocabulary()
-    vocab_toks.add_word('<start>')
-    vocab_toks.add_word('<end>')
-    vocab_toks.add_word('<eoi>')
+    unit_toks = Vocabulary()
+    unit_toks.add_word('<start>')
+    unit_toks.add_word('<end>')
+    unit_toks.add_word('<eoi>')
 
     # Add the words to the vocabulary.
-    for i, word in enumerate(words):
-        vocab_toks.add_word(word)
-    vocab_toks.add_word('<pad>')
+    for i, word in enumerate(unit):
+        unit_toks.add_word(word)    
+    unit_toks.add_word('<pad>')
 
     # Ingredient vocab
     # Create a vocab wrapper for ingredients
     vocab_ingrs = Vocabulary()
     idx = vocab_ingrs.add_word('<end>')
+
     # this returns the next idx to add words to
     # Add the ingredients to the vocabulary.
     for k, _ in ingrs.items():
@@ -247,72 +323,8 @@ def build_vocab(dir_file):
     _ = vocab_ingrs.add_word('<pad>', idx)
 
     print("Total ingr vocabulary size: {}".format(len(vocab_ingrs)))
-    print("Total token vocabulary size: {}".format(len(vocab_toks)))
 
-    dataset = {'train': [], 'val': [], 'test': []}
-
-    for i, entry in tqdm(enumerate(layer1)):
-
-        # get all instructions for this recipe
-        instrs = entry['instructions']
-
-        instrs_list = []
-        ingrs_list = []
-        images_list = []
-
-        # retrieve pre-detected ingredients for this entry
-        det_ingrs = dets[id2ingr_pos[entry['id']]]['ingredients']
-        valid = dets[id2ingr_pos[entry['id']]]['valid']
-        labels = []
-
-        for j, det_ingr in enumerate(det_ingrs):
-            if len(det_ingr) > 0 and valid[j]:
-                det_ingr_undrs = get_ingredient(det_ingr, replace_Ingrs)
-                ingrs_list.append(det_ingr_undrs)
-                label_idx = vocab_ingrs(det_ingr_undrs)
-                if label_idx is not vocab_ingrs('<pad>') and label_idx not in labels:
-                    labels.append(label_idx)
-
-        # get raw text for instructions of this entry
-        acc_len = 0
-        for instr in instrs:
-            instr = get_instruction(instr, replace_dict_instrs)
-            if len(instr) > 0:
-                acc_len += len(instr)
-                instrs_list.append(instr)
-
-        # we discard recipes with too many or too few ingredients or instruction words
-
-        if len(labels) < 2 or len(instrs_list) < 2 \
-                or len(instrs_list) >= 20 or len(labels) >= 20 \
-                or acc_len < 20:
-            continue
-
-        if entry['id'] in id2im_pos.keys():
-            ims = layer2[id2im_pos[entry['id']]]
-
-            # copy image paths for this recipe
-            for im in ims['images']:
-                images_list.append(im['id'])
-
-        # tokenize sentences
-        toks = []
-
-        for instr in instrs_list:
-            tokens = nltk.tokenize.word_tokenize(instr)
-            toks.append(tokens)
-
-        title = nltk.tokenize.word_tokenize(entry['title'].lower())
-
-        newentry = {'id': entry['id'], 'instructions': instrs_list, 'tokenized': toks,
-                    'ingredients': ingrs_list, 'images': images_list, 'title': title}
-        dataset[entry['partition']].append(newentry)
-
-    print('Dataset size:')
-    for split in dataset.keys():
-        print(split, ':', len(dataset[split]))
-
-    return vocab_ingrs, vocab_toks, dataset
+    return vocab_ingrs, unit_toks, dataset
 
 class Vocabulary(object):
     def __init__(self):
@@ -344,14 +356,48 @@ class Vocabulary(object):
     def __len__(self):
         return len(self.idx2word)
 
+def simplify_ingrs(_unit, ingr_candidate):
+    replace_units = {'cup', 'tablespoon', 'teaspoon', 'teaspoonp', 'lb', 'ounces', 'can', 'large', 'medium', 'whole',
+                    'pound', 'pounds', 'small', 'package', 'g', 'lbs', 'ounce', 'pinch', 'grams', 'cans', 'can',
+                    'ml', 'dash', 'pkg', 'oz', 'clove', 'bunch', 'packages', 'jar', 'box', 'quart', 'stick',
+                    'bag', 'qt', 'tbs', 'bottle'}
+    det_unit = _unit.lower()
+    part = det_unit.split('_')
+    unit_word = ''
+    quantity = ''
+
+    #find unit of ingredient
+    for word in replace_units:
+        if word in part:
+            unit_word = word
+
+    #find quantity of ingredient
+    for i, item in enumerate(det_unit.split('_')):
+        if len(item) > 0:
+            if item[0].isdigit():
+                quantity = part[i]
+                break
+    
+    if len(quantity) > 0 and len(unit_word) > 0:
+        det_unit = quantity + '_' + unit_word + '_' + ingr_candidate
+    elif not len(quantity) > 0 and len(unit_word) > 0:
+        det_unit = '1' + '_' + unit_word + '_' + ingr_candidate
+    elif len(quantity) > 0 and not len(unit_word) > 0:
+        det_unit = quantity + '_' + ingr_candidate
+    else:
+        det_unit = '1_' + ingr_candidate
+    
+    return det_unit
+
 def main(dir_file):
     # build_vocab(dir_file)
-    vocab_ingrs, vocab_toks, dataset = build_vocab(dir_file)
+    vocab_ingrs, vocab_unit, dataset = build_vocab(dir_file)
 
     with open(os.path.join(dir_file + 'recipe1m_vocab_ingrs.pkl'), 'wb') as f:
         pickle.dump(vocab_ingrs, f)
-    with open(os.path.join(dir_file +'recipe1m_vocab_toks.pkl'), 'wb') as f:
-        pickle.dump(vocab_toks, f)
+
+    with open(os.path.join(dir_file + 'recipe1m_vocab_unit.pkl'), 'wb') as f:
+        pickle.dump(vocab_unit, f)
 
     for split in dataset.keys():
         with open(os.path.join(dir_file + 'recipe1m_' + split + '.pkl'), 'wb') as f:
