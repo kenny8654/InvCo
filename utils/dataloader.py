4695c63609befdbbd227cbef914e38354bc802b6
Timmy <apsttimmy@gmail.com>
2019-12-18 15:05:06 +0800

    (fix): data

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -7,15 +7,18 @@ import numpy as np
 from utils.ingrs_vocab import Vocabulary
 
 class RecipeDataset(data.Dataset):
-    def __init__(self,dir_file,max_num_samples=-1,maxnumims=5,max_num_labels=20,split='train'):
+    def __init__(self,dir_file,max_num_samples=-1,maxnumims=5,max_num_labels=20,max_unit_len=150,split='train'):
         
         self.maxnumims = maxnumims              # maximum number of images per recipe
         self.max_num_labels = max_num_labels    # max num of ingredients per recipe
+        self.max_unit_len = max_unit_len
         self.split = split                      # train , val or test
+
+        self.dir_file = dir_file
                
-        self.ingrs_vocab = pk.load(open(os.path.join(dir_file,'recipe1m_vocab_ingrs.pkl'), 'rb'))  # ingredients and their indexes
-        self.instrs_vocab = pk.load(open(os.path.join(dir_file,'recipe1m_vocab_toks.pkl'), 'rb'))  # single words and their indexes
-        self.dataset = pk.load(open(os.path.join(dir_file,'recipe1m_'+split+'.pkl'), 'rb'))             # every recipe (id,instructions,tokenized,ingredients,images,title)
+        #self.ingrs_vocab = pk.load(open(os.path.join(dir_file,'recipe1m_vocab_ingrs.pkl'), 'rb'))  # ingredients and their indexes
+        self.vocab_unit = pk.load(open(os.path.join(dir_file,'recipe1m_vocab_unit.pkl'),'rb'))     # units , tokens and their indexes
+        self.dataset = pk.load(open(os.path.join(dir_file,'recipe1m_'+ split +'.pkl'), 'rb'))      # every recipe (id,instructions,tokenized,ingredients,images,title)
 
         # filter recipes that don't have image 
         self.ids = []
@@ -25,7 +28,7 @@ class RecipeDataset(data.Dataset):
             self.ids.append(i)                   # ids save all recipes with images
         
         #print('ids:',self.ids)
-        #print('len(ids):',len(self.ids))
+        print('len(ids):',len(self.ids))
 
         if max_num_samples != -1:
             random.shuffle(self.ids)
@@ -33,61 +36,49 @@ class RecipeDataset(data.Dataset):
 
     def __getitem__(self,index):
 
-        # get a recipe according to index 
+        # get a recipe according to index
         recipe = self.dataset[self.ids[index]]
 
         # read attributes from this recipe
         recipe_id = recipe['id']
-        instr_words = recipe['tokenized']
         img_paths = recipe['images'][0:self.maxnumims]
-        ingrs = self.dataset[self.ids[index]]['ingredients']
+        
+        #ingrs = recipe['ingredients']
         title = recipe['title']
 
-        #print('recipe_id:',recipe_id)
-        #print('instr_words:',instr_words)
-        #print('img_paths:',img_paths)
-        #print('ingrs:',ingrs)
-        #print('title:',title)
+        # ingredients with units
+        ingrs_unit = recipe['ingrs_unit'] 
 
-        # init the index array
-        ingrs_idx = np.ones(self.max_num_labels) * self.ingrs_vocab('<pad>')
-
-        pos = 0
-        for i in range(self.max_num_labels):
-            
-            if i >= len(ingrs):                 # len(labels) : num of ingredients of this recipe
-                ingr = '<pad>'  
-            else :
-                ingr = ingrs[i]
-            label_idx = self.ingrs_vocab(ingr)  # get the index of this ingredient 
-            
-            # append(?) the index to array , and avoid duplication
-            if label_idx not in ingrs_idx:
-                ingrs_idx[pos] = label_idx
-                pos += 1
-
-        # add index of '<end>' (0) at the end of ingredients
-        ingrs_idx[pos] = self.ingrs_vocab('<end>')
-
-        #print('ingrs_idx:',ingrs_idx)
-
-        labels = torch.from_numpy(ingrs_idx).long()
-
-        #print('labels:',labels)
-
+        # shuffle the images
         if self.split == 'train':
             img_idx = np.random.randint(0, len(img_paths))
         else:
             img_idx = 0
+
+        # the chosen img
         path = img_paths[img_idx]
         
-        image_dir = os.path.join(path[0], path[1], path[2], path[3], path)
+        image_dir = os.path.join(self.dir_file,path[0], path[1], path[2], path[3], path)
 
         #print('image_dir:',image_dir)
 
         image = self.read_img(image_dir)
 
-        return image,labels
+        unit_idx = np.ones(self.max_unit_len) * self.vocab_unit('<pad>')
+        pos2 = 0
+        # unit to idx
+        for sentence in ingrs_unit:
+            for token in sentence.split('_'):
+                idx = self.vocab_unit(token)
+                #print(idx,' : ',self.vocab_unit.idx2word[idx])
+                unit_idx[pos2] = idx
+                pos2 += 1
+                if pos2 >= self.max_unit_len:
+                    break
+
+        labels_unit = torch.from_numpy(unit_idx).long()
+
+        return image,labels_unit #,labels
 
     def __len__(self):
         return len(self.ids)
@@ -101,7 +92,7 @@ class RecipeDataset(data.Dataset):
         img = img_dir
         return img
 
-def get_loader(dir_file,split,batch_size=16,shuffle=False):
+def get_loader(dir_file,split,batch_size=4,shuffle=False):
     dataset = RecipeDataset(dir_file,split=split)
 
     RecipeLoader = data.DataLoader(dataset=dataset,\
